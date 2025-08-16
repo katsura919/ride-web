96f06e7f69270ad75fa2a6efab80c699c78f76b1
katsura919 <katsuragik919@gmail.com>
2025-08-16 17:21:17 +0800

    feat: update landing page and add new components

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -1,444 +1,70 @@
 import { motion } from "framer-motion";
 import { World } from "../ui/globe";
 import { FlipWords } from "../ui/flip-words";
+import { globeConfig, sampleArcs } from "../../data/globe-data";
 
 export default function HeroSection() {
-  // Globe config and sample data
-  const globeConfig = {
-    pointSize: 4,
-    globeColor: "#062056",
-    showAtmosphere: true,
-    atmosphereColor: "#FFFFFF",
-    atmosphereAltitude: 0.1,
-    emissive: "#062056",
-    emissiveIntensity: 0.1,
-    shininess: 0.9,
-    polygonColor: "rgba(255,255,255,0.7)",
-    ambientLight: "#38bdf8",
-    directionalLeftLight: "#ffffff",
-    directionalTopLight: "#ffffff",
-    pointLight: "#ffffff",
-    arcTime: 1000,
-    arcLength: 0.9,
-    rings: 1,
-    maxRings: 3,
-    initialPosition: { lat: 22.3193, lng: 114.1694 },
-    autoRotate: true,
-    autoRotateSpeed: 100,
-  };
-  const colors = ["#06b6d4", "#3b82f6", "#6366f1"];
-  const sampleArcs = [
-    {
-      order: 1,
-      startLat: -19.885592,
-      startLng: -43.951191,
-      endLat: -22.9068,
-      endLng: -43.1729,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 1,
-      startLat: 28.6139,
-      startLng: 77.209,
-      endLat: 3.139,
-      endLng: 101.6869,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 1,
-      startLat: -19.885592,
-      startLng: -43.951191,
-      endLat: -1.303396,
-      endLng: 36.852443,
-      arcAlt: 0.5,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 2,
-      startLat: 1.3521,
-      startLng: 103.8198,
-      endLat: 35.6762,
-      endLng: 139.6503,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 2,
-      startLat: 51.5072,
-      startLng: -0.1276,
-      endLat: 3.139,
-      endLng: 101.6869,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 2,
-      startLat: -15.785493,
-      startLng: -47.909029,
-      endLat: 36.162809,
-      endLng: -115.119411,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 3,
-      startLat: -33.8688,
-      startLng: 151.2093,
-      endLat: 22.3193,
-      endLng: 114.1694,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 3,
-      startLat: 21.3099,
-      startLng: -157.8581,
-      endLat: 40.7128,
-      endLng: -74.006,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 3,
-      startLat: -6.2088,
-      startLng: 106.8456,
-      endLat: 51.5072,
-      endLng: -0.1276,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 4,
-      startLat: 11.986597,
-      startLng: 8.571831,
-      endLat: -15.595412,
-      endLng: -56.05918,
-      arcAlt: 0.5,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 4,
-      startLat: -34.6037,
-      startLng: -58.3816,
-      endLat: 22.3193,
-      endLng: 114.1694,
-      arcAlt: 0.7,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 4,
-      startLat: 51.5072,
-      startLng: -0.1276,
-      endLat: 48.8566,
-      endLng: -2.3522,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 5,
-      startLat: 14.5995,
-      startLng: 120.9842,
-      endLat: 51.5072,
-      endLng: -0.1276,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 5,
-      startLat: 1.3521,
-      startLng: 103.8198,
-      endLat: -33.8688,
-      endLng: 151.2093,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 5,
-      startLat: 34.0522,
-      startLng: -118.2437,
-      endLat: 48.8566,
-      endLng: -2.3522,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 6,
-      startLat: -15.432563,
-      startLng: 28.315853,
-      endLat: 1.094136,
-      endLng: -63.34546,
-      arcAlt: 0.7,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 6,
-      startLat: 37.5665,
-      startLng: 126.978,
-      endLat: 35.6762,
-      endLng: 139.6503,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 6,
-      startLat: 22.3193,
-      startLng: 114.1694,
-      endLat: 51.5072,
-      endLng: -0.1276,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 7,
-      startLat: -19.885592,
-      startLng: -43.951191,
-      endLat: -15.595412,
-      endLng: -56.05918,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 7,
-      startLat: 48.8566,
-      startLng: -2.3522,
-      endLat: 52.52,
-      endLng: 13.405,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 7,
-      startLat: 52.52,
-      startLng: 13.405,
-      endLat: 34.0522,
-      endLng: -118.2437,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 8,
-      startLat: -8.833221,
-      startLng: 13.264837,
-      endLat: -33.936138,
-      endLng: 18.436529,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 8,
-      startLat: 49.2827,
-      startLng: -123.1207,
-      endLat: 52.3676,
-      endLng: 4.9041,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 8,
-      startLat: 1.3521,
-      startLng: 103.8198,
-      endLat: 40.7128,
-      endLng: -74.006,
-      arcAlt: 0.5,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 9,
-      startLat: 51.5072,
-      startLng: -0.1276,
-      endLat: 34.0522,
-      endLng: -118.2437,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 9,
-      startLat: 22.3193,
-      startLng: 114.1694,
-      endLat: -22.9068,
-      endLng: -43.1729,
-      arcAlt: 0.7,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 9,
-      startLat: 1.3521,
-      startLng: 103.8198,
-      endLat: -34.6037,
-      endLng: -58.3816,
-      arcAlt: 0.5,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 10,
-      startLat: -22.9068,
-      startLng: -43.1729,
-      endLat: 28.6139,
-      endLng: 77.209,
-      arcAlt: 0.7,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 10,
-      startLat: 34.0522,
-      startLng: -118.2437,
-      endLat: 31.2304,
-      endLng: 121.4737,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 10,
-      startLat: -6.2088,
-      startLng: 106.8456,
-      endLat: 52.3676,
-      endLng: 4.9041,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 11,
-      startLat: 41.9028,
-      startLng: 12.4964,
-      endLat: 34.0522,
-      endLng: -118.2437,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 11,
-      startLat: -6.2088,
-      startLng: 106.8456,
-      endLat: 31.2304,
-      endLng: 121.4737,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 11,
-      startLat: 22.3193,
-      startLng: 114.1694,
-      endLat: 1.3521,
-      endLng: 103.8198,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 12,
-      startLat: 34.0522,
-      startLng: -118.2437,
-      endLat: 37.7749,
-      endLng: -122.4194,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 12,
-      startLat: 35.6762,
-      startLng: 139.6503,
-      endLat: 22.3193,
-      endLng: 114.1694,
-      arcAlt: 0.2,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 12,
-      startLat: 22.3193,
-      startLng: 114.1694,
-      endLat: 34.0522,
-      endLng: -118.2437,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 13,
-      startLat: 52.52,
-      startLng: 13.405,
-      endLat: 22.3193,
-      endLng: 114.1694,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 13,
-      startLat: 11.986597,
-      startLng: 8.571831,
-      endLat: 35.6762,
-      endLng: 139.6503,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 13,
-      startLat: -22.9068,
-      startLng: -43.1729,
-      endLat: -34.6037,
-      endLng: -58.3816,
-      arcAlt: 0.1,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-    {
-      order: 14,
-      startLat: -33.936138,
-      startLng: 18.436529,
-      endLat: 21.395643,
-      endLng: 39.883798,
-      arcAlt: 0.3,
-      color: colors[Math.floor(Math.random() * (colors.length - 1))],
-    },
-  ];
+  // ...existing code...
   return (
-    <section className="h-[70vh] mt-20  w-full py-16 px-4 sm:py-20 sm:px-6 relative  "id="hero">
-
-      <div className="h-100 max-w-7xl mx-auto ">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-10 items-center overflow-visible">
-        {/* Left Content */}
-        <motion.div
-          initial={{ opacity: 0, y: 40 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-          className="text-center lg:text-left flex flex-col items-center lg:items-start"
-        >
-          
-          <div className="mt-2 mb-8 w-full">
-            <div className="font-bold text-whit
-              text-5xl sm:text-10xl md:text-6xl lg:text-7xl
-              leading-tight sm:leading-tight md:leading-tight lg:leading-tight
-              break-words w-full ">
-              Move <span className="inline-block bg-neutral-800 bg-opacity-80 rounded-lg px-4 py-1 text-white shadow-md align-middle border"><FlipWords words={["smarter", "safer", "easier", "together",]} /></span> <br />
-              with Ride Alert!
-            </div>
-            <div className="mt-4 text-neutral-600 dark:text-neutral-400 text-lg sm:text-2xl font-normal w-full">
-              Track vehicles in real time and get instant alerts.
-              Manage trips and safety—all in one platform.
-            </div>
+    <section className="min-h-screen w-full py-0 pt-25 px-0 relative flex items-center justify-center" id="hero">
+      {/* Grid Background for whole hero section */}
+      <div
+        className={`
+          absolute inset-0 -z-10
+          [background-size:40px_40px]
+          [background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]
+          dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]
+        `}
+      />
+      {/* Mask for faded effect */}
+      <div className="pointer-events-none absolute inset-0 -z-10 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <div className="relative w-full h-[70vh] flex items-center justify-center">
+        <div className="h-full w-full max-w-7xl mx-auto flex items-center justify-center">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-10 items-center overflow-visible w-full">
+            {/* Left Content */}
+            <motion.div
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              className="px-10 text-center lg:text-left flex flex-col items-center lg:items-start"
+            >
+              <div className="mt-2  w-full">
+                <div className="font-bold text-whit
+                  text-5xl sm:text-10xl md:text-6xl lg:text-1xl
+                  leading-tight sm:leading-tight md:leading-tight lg:leading-tight
+                  break-words w-full ">
+                  Move <span className="inline-block  bg-neutral-800 bg-opacity-80 rounded-lg px-4 py-1 text-white shadow-md align-middle border"><FlipWords words={["smarter", "safer", "easier", "together",]} /></span> <br />
+                  with Ride Alert!
+                </div>
+                <div className="mt-4 text-neutral-600 dark:text-neutral-400 text-lg sm:text-2xl font-normal w-full">
+                  Track vehicles in real time and get instant alerts.
+                  Manage trips and safety—all in one platform.
+                </div>
+              </div>
+              <div className="flex flex-row gap-4 w-full sm:w-auto justify-center lg:justify-start">
+                <button className="bg-white text-black px-4 py-2 sm:px-8 sm:py-4 rounded-xl font-mediumshadow-sm w-auto text-sm sm:text-base cursor-pointer">
+                  Start Tracking
+                </button>
+                <button className="border border-gray-300 text-white px-4 py-2 sm:px-8 sm:py-4 rounded-xl font-medium w-auto text-sm sm:text-base cursor-pointer">
+                  Watch Demo
+                </button>
+              </div>
+            </motion.div>
+            {/* Right Content - Mobile App Mockup */}
+            <motion.div
+              initial={{ opacity: 0, x: 40 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+              className="relative flex justify-center  w-full overflow-visible"
+            >
+              {/* Globe Visualization */}
+              <div className="w-full h-[380px] sm:h-[300px] md:h-[400px] lg:h-[600px] overflow-visible">
+                <World data={sampleArcs} globeConfig={globeConfig} />
+              </div>
+            </motion.div>
           </div>
-    <div className="flex flex-row gap-4 w-full sm:w-auto justify-center lg:justify-start">
-            <button className="bg-white text-black px-4 py-2 sm:px-8 sm:py-4 rounded-xl font-mediumshadow-sm w-auto text-sm sm:text-base cursor-pointer">
-              Start Tracking
-            </button>
-            <button className="border border-gray-300 text-white px-4 py-2 sm:px-8 sm:py-4 rounded-xl font-medium w-auto text-sm sm:text-base cursor-pointer">
-              Watch Demo
-            </button>
-          </div>
-        </motion.div>
-
-        {/* Right Content - Mobile App Mockup */}
-        <motion.div
-          initial={{ opacity: 0, x: 40 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-          className="relative flex justify-center  w-full overflow-visible"
-        >
-          {/* Globe Visualization */}
-          <div className="w-full h-[380px] sm:h-[300px] md:h-[400px] lg:h-[600px] overflow-visible">
-            <World data={sampleArcs} globeConfig={globeConfig} />
-          </div>
-        </motion.div>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 }
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -2,9 +2,10 @@ import HeroSection from "../components/ui/hero-section";
 import FeaturesSectionDemo from "../components/features-section";
 import Footer from "../components/ui/footer";
 import NavBar from "../components/ui/nav-bar";
-import { PlaceholdersAndVanishInput } from "../components/ui/placeholders-and-vanish-input";
+
 import { ContainerScroll } from "../components/ui/container-scroll-animation";
 import carDashboardImg from "../assets/home-demo.png";
+
 
 export default function Landing() {
   return (
@@ -29,7 +30,7 @@ export default function Landing() {
                     <h1 className="text-4xl font-semibold text-black dark:text-white">
                       Unleash the power of <br />
                       <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                        Scroll Animations
+                        Smart Dashboard
                       </span>
                     </h1>
                   </>
@@ -47,17 +48,6 @@ export default function Landing() {
             </div>
           </section>
           <FeaturesSectionDemo />
-          {/* Ask Anything Section */}
-          <section className="w-full flex flex-col items-center justify-center pb-35 px-10 bg-black">
-            <h2 className="text-white text-3xl sm:text-5xl font-semibold mb-10 text-center">Ask Ride Alert Anything</h2>
-            <div className="w-full max-w-xl mx-auto">
-              <PlaceholdersAndVanishInput
-                placeholders={["Who are the Devs?", "How does Ride Alert work?", "Show me the routes", "What are the latest features?", "How do I register a vehicle?"]}
-                onChange={() => {}}
-                onSubmit={() => {}}
-              />
-            </div>
-          </section>
         </main>
         <Footer />
       </div>
